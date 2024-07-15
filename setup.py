ac45ce7ab2c2c87ec96fd60e048b9ffe77f16414
maxlevelnpc <furry609@gmail.com>
2024-07-15 21:51:04 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test-123',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Olrox',
+    author_email='your.email@example.com',
+    description='A brief description of your package',
+    url='https://github.com/maxlevelnpc/test-project',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
